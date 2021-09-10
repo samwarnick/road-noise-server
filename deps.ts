93a3ad212a0edc41aec015bf36bb37cf5a258c83
Sam Warnick <sam.t.warnick@gmail.com>
2021-09-10 09:28:34 -0400

    Add simple routes

--- deps.ts
+++ deps.ts
@@ -2,3 +2,4 @@ export {
   Application,
   Router,
 } from "https://github.com/samwarnick/Groundwork/raw/main/mod.ts";
+export { ensureFile } from "https://deno.land/std@0.106.0/fs/mod.ts";
--- server.ts
+++ server.ts
@@ -1,7 +1,29 @@
-import { Application, Router } from "./deps.ts";
+import { Application, Router, ensureFile } from "./deps.ts";
+import { loadEnv } from "./utils.ts";
+
+await loadEnv();
+const TOKEN = Deno.env.get("TOKEN");
 
 const app = new Application();
 
 const router = new Router();
+router.get("/", async () => {
+  const json = await Deno.readTextFile("_data/data.json");
+  return new Response(json, {
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+});
+router.post("/", (request) => {
+  const token = request.headers.get("Authorization");
+  if (token !== `Bearer ${TOKEN}`) {
+    return new Response(null, { status: 401 });
+  }
+  return new Response();
+});
 
+await ensureFile("_data/data.json");
+
+app.use(router);
 app.start({ port: 7575 });
